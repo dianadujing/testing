5b4ddba2d16e3f5e48c35b2285aba908b12c7501
Sam Tsai <TsaiSa@ae.com>
2013-08-01 17:20:47 -0400

    Clean up test structure

--- tests/asapTest.js
+++ tests/asapTest.js
@@ -1,21 +1,20 @@
-var should = require('should')
-   ,assert = require('assert')
-   ,expect = require('expect.js')
-   //,cheerio = require('cheerio')
-   ,myCode = require('../asap/asap.js');
+var should = require('should'),
+    assert = require('assert'),
+    expect = require('expect.js'),
+    asapLib = require('../asap/asap.js');
+
 describe('ASAP library ', function(){
   describe('asap ', function(){
     it('should be a function', function(){
-      myCode.asap.should.be.a('function');
-    })
+      asapLib.asap.should.be.a('function');
+    });
     it('should have a property called loadScript', function(){
-      myCode.asap.should.have.property('loadScript');
-    })
+      asapLib.asap.should.have.property('loadScript');
+    });
     it('should return a json object', function(){
-      myCode.asap('bagToken').should.have.property('resolve');
-    })
+      asapLib.asap('bagToken').should.have.property('resolve');
+    });
     it('should have registry', function(){
-    })
-
-  })
-})
+    });
+  });
+});
