42e5e15f053018e6065fb8c35016cf7673303835
Du, Jing <DuJ@AE.com>
2013-08-06 13:14:58 -0400

    revised test

--- tests/asapDOMTest.js
+++ tests/asapDOMTest.js
@@ -1,34 +1,38 @@
-var doc = document,
-      t = doc.createElement("script"),
-      s = doc.getElementsByTagName("script")[0];
-function loadScript( url, id ) {
-    if (url) {
-      if (id) {
-        if (doc.getElementById(id)) {return;}
-        t.id = id;
-      }
+describe('asap', function(){
 
-      t.type = "text/javascript";
-      t.async = false;
-      t.src = url;
-      s.parentNode.insertBefore(t, s);
-    }
-  }
+  asap.loadScript('/coverage.html','node1');
+  var node1 = document.getElementById('node1');
+  asap.loadScript('/coverage.html','node2');
+  var node2 = document.getElementById('node2');
 
-describe('loadScript', function(){
-      it(' should have doc: ', function(){
-        expect(doc).to.not.equal(null);
+  describe('loadScript', function(){
+    it(' should assign value to variables: ', function(){
+      (node1.id).should.equal('node1');
+    });
+  });
+
+  describe('resolve and then - ', function(){
+    var asap_result = asap(node1.id).resolve();
+
+    it(' resolve should be an object', function(){
+      (asap_result).should.be.a('object');
+    });
+
+    it(' should return the same api object: ', function(){
+        (asap_result.resolve()).should.be.equal(asap_result);
+    });
+
+    it(' then() should work after resolve(): ', function(){
+      asap(node1.id).then(function(){
+        console.log('succeed!');
       });
+    });
 
-      it(' should have script element: ', function(){
-        expect(s).to.not.equal(null);
+    it(' then() cannot work before resolve(): ', function(){
+      asap(node2.id).then(function(){
+        console.log('failed!');
       });
+    });
 
-      it(' should assign value to variables: ', function(){
-        loadScript('coverage.html','abc');
-        expect(t.id).to.equal('abc');
-        console.log(t.src);
-        console.log(t.id);
-
-      });
-    })
+  });
+});
